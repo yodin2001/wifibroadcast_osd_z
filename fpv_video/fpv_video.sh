42861179dc774dce08c8346074db8df88ec4d7cc
yodin2001 <yodin.2001@gmail.com>
2022-02-04 22:55:40 +0300

    Add fpv_video

---
+++ fpv_video/fpv_video.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+set -o pipefail
+gst-launch-1.0 udpsrc port=5600 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! 'video/x-h264,stream-format=byte-stream' ! fdsink | fpv_video
---
+++ fpv_video/rpi_setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e
+set -x
+
+apt-get update
+apt-get install build-essential libraspberrypi0 libraspberrypi-bin gstreamer1.0-tools gstreamer1.0-plugins-good
+
+make mode=rpi codec=h264
+
+cp -a osd fpv_video/{fpv_video,fpv_video.sh} /usr/bin
+
+cat > /etc/systemd/system/osd.service <<EOF
+[Unit]
+Description=FPV OSD
+After=network-online.target
+
+[Service]
+ExecStart=/bin/openvt -s -e -- bash -c 'TERM=linux setterm --blank force --clear all --cursor off /dev/tty && exec /usr/bin/osd'
+Type=simple
+Restart=always
+RestartSec=1s
+TimeoutStopSec=10s
+KillMode=control-group
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat > /etc/systemd/system/fpv-video.service <<EOF
+[Unit]
+Description=FPV video
+After=network-online.target
+
+[Service]
+ExecStart=/usr/bin/fpv_video.sh
+Type=simple
+Restart=always
+RestartSec=1s
+TimeoutStopSec=10s
+KillMode=control-group
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable fpv-video
+systemctl enable osd
+systemctl restart fpv-video
+systemctl restart osd
